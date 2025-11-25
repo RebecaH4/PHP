89fb026d61d6cab133cce5ee8ed9112b80a4c49c
Rebe HC <hernandezcruzrebeca767@gmail.com>
2025-11-24 19:57:30 -0600

    Class

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+        <?php
+        echo "Hola, soy un script PHP!";
+        ?>
+    </body>
+</html>
